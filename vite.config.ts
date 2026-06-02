0109f558da8b774adca5856ef835eacc4ea36ae1
GaloszJakub <kubagalosz@gmail.com>
2026-06-02 10:35:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/carrer-rekord/',
+    base: process.env.VERCEL ? '/' : '/carrer-rekord/',
     server: {
       port: 3000,
       host: '0.0.0.0',
